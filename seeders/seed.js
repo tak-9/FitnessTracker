0a3ef52f909e5be7392b50015993b757ebc32411
Takuji Okubo <takuji.okubo@gmail.com>
2020-03-19 12:35:39 +1100

    For heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,12 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 let workoutSeed = [
   {
