a1db4fa63cceea10c2143387a278916f5d1a3985
Takuji Okubo <takuji.okubo@gmail.com>
2020-03-19 20:28:14 +1100

    Removed unnecessary file, comments

--- models/Workout.js
+++ models/Workout.js
@@ -2,35 +2,6 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-/*
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        unique: true
-    },
-    exercises: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "Excercise"
-        }
-      ]
-});
-*/
-/*
-const workoutSchema = new Schema({
-    day: Date, 
-    exercises: [{
-        type: String,
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number
-  }]
-});
-*/
-
 const ExcerciseSchema = new Schema({
     type: String,
     name: String,
--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  //Excercise: require("./Excercise"),
   Workout: require("./Workout")
 };
